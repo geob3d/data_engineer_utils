c891672581464dfe32df513bd1996b358b983b59
George <geob3d@gmail.com>
2024-05-14 16:31:29 -0400

    Update __init__.py
    Added get_upsert_type_by_dbms

--- data_engineer_utils/__init__.py
+++ data_engineer_utils/__init__.py
@@ -4,6 +4,7 @@ __version__ = '0.2.0'
 from data_engineer_utils.dbms_support_map import (
     dbms_list,
     get_dbms_by_py_driver,
+    get_upsert_type_by_dbms,
 )
 from data_engineer_utils.file_support import FileSupport
 from data_engineer_utils.gen_connection_string import CreateConnectionString
