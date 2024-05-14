3957acd2a89a80674c671e2f60ee25b47bd9ce8c
George <geob3d@gmail.com>
2024-05-14 16:11:09 -0400

    Added dbms_suppport_map.py
    The file maps python drivers to python dbms names. The file includes the upsert_type for each dbms

--- data_engineer_utils/__init__.py
+++ data_engineer_utils/__init__.py
@@ -1,10 +1,13 @@
 __version__ = '0.2.0'
 
 
+from data_engineer_utils.dbms_support_map import (
+    dbms_list,
+    get_dbms_by_py_driver,
+)
 from data_engineer_utils.file_support import FileSupport
 from data_engineer_utils.gen_connection_string import CreateConnectionString
 from data_engineer_utils.gen_migration_num import create_folder_migration_num
-from data_engineer_utils.raw_sql_execution import execute_sql_statment
 from data_engineer_utils.support import (
     concat_dict_to_delimated_list,
     schema_formatter,
---
+++ data_engineer_utils/dbms_support_map.py
@@ -0,0 +1,68 @@
+sqlalchemy_drivers_merge_systems = {
+    "oracle": {"drivers": ["oracle+cx_oracle", "oracle+oracledb", "oracle+pyodbc"], "upsert_type": "MERGE"},
+    "mssql": {"drivers": ["mssql+pyodbc", "mssql+pymssql", "mssql+adodbapi", "mssql+pytds"], "upsert_type": "MERGE"},
+    "db2": {"drivers": ["ibm_db_sa", "db2+ibm_db", "db2+pyodbc"], "upsert_type": "MERGE"},
+    "postgresql": {
+        "drivers": [
+            "postgresql+psycopg2",
+            "postgresql+pg8000",
+            "postgresql+asyncpg",
+            "postgresql+pygresql",
+            "postgresql+psycopg",
+        ],
+        "upsert_type": {
+            "default": "ON CONFLICT",  # For versions before 15
+            "15+": "MERGE",  # For versions 15 and above
+        },
+    },
+    "mysql": {
+        "drivers": [
+            "mysql+mysqlconnector",
+            "mysql+pymysql",
+            "mysql+mysqldb",
+            "mysql+pyodbc",
+            "mysql+cymysql",
+            "mysql+aiomysql",
+        ],
+        "upsert_type": "ON DUPLICATE KEY UPDATE",
+    },
+    "sqlite": {"drivers": ["sqlite+pysqlite"], "upsert_type": "ON CONFLICT"},
+    "teradata": {"drivers": ["teradata+teradata"], "upsert_type": "MERGE"},
+    "hana": {"drivers": ["hana+pyhdb", "hana+hdbcli"], "upsert_type": "MERGE"},
+    "snowflake": {"drivers": ["snowflake+snowflake"], "upsert_type": "MERGE"},
+    "redshift": {"drivers": ["redshift+psycopg2", "redshift+redshift_connector"], "upsert_type": "MERGE"},
+    "bigquery": {"drivers": ["bigquery+bigquery"], "upsert_type": "MERGE"},
+}
+
+
+# Additional mssql_local entry for local temporary table in MSSQL
+# sqlalchemy_drivers_merge_systems["mssql_global"] = {
+#     "drivers": ["mssql+pyodbc", "mssql+pymssql", "mssql+adodbapi", "mssql+pytds"],
+#     "create_temp_table_header": "CREATE TABLE ##{table_name} ",
+#     "upsert_type": "MERGE"
+# }
+
+dbms_list = list(sqlalchemy_drivers_merge_systems.keys())
+
+
+def get_dbms_by_py_driver(driver_name) -> str:
+    """
+    Finds the DBMS key based on the given SQLAlchemy driver.
+
+    Args:
+        target_value (str): The SQLAlchemy driver string to search for.
+
+    Returns:
+        Optional[str]: The DBMS key if found, otherwise None.
+
+    Raises:
+        ValueError: If the target_value is not found in any DBMS drivers.
+    """
+    dbms_key = next(
+        (key for key, value in sqlalchemy_drivers_merge_systems.items() if driver_name in value["drivers"]), None
+    )
+
+    if dbms_key is None:
+        raise ValueError(f"The target value {driver_name} is not found in any DBMS drivers.")
+
+    return dbms_key
